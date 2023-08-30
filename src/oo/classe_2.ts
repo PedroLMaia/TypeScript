1416bd176855fe8a1c9e7d451cfa1d3b0322f191
PedroLMaia <pedro-luiz-maia@hotmail.com>
2023-08-30 13:56:17 -0300

    Atribuição via Construtor

--- src/oo/classe_2.ts
+++ src/oo/classe_2.ts
@@ -1,16 +1,16 @@
-class Curso2 {
-    readonly id: number
-    public nome: string | undefined
-
-    constructor(id: number) {
-        this.id = id
+class Curso {
+    constructor(readonly id: number, public nome?: string) {
         if(id < 1) throw new Error('Id invalido')
     }
 }
 
-const c1 = new Curso2(123)
-// c1.id = 456
-c1.nome = 'Iniciando com JAVA'
+const c1 = new Curso(123)
+c1.nome = 'Iniciando com Typescript'
 console.log(c1.id)
 console.log(c1.nome)
+
+const c2 = new Curso(456, 'Javascript')
+console.log(c2.id)
+console.log(c2.nome)
+
 export {}
