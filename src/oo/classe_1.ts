8475458fd6076055cd5d9e6fb7ab8cdcff861878
PedroLMaia <pedro-luiz-maia@hotmail.com>
2023-08-30 12:53:34 -0300

    metodos publicos e privados

--- src/oo/classe_1.ts
+++ src/oo/classe_1.ts
@@ -1,11 +1,20 @@
 class Curso {
-    nome: string | undefined
+    private _id: number | undefined
+    public nome: string | undefined
+
+    get id() {
+        return this._id
+    }
+
+    set id(id: number | undefined) {
+        if((id ?? 0) > 0) { //id ?? 0 -> se id for nulo ou undefined, o valor dele sera atribuido 0, entao ele fara a comparacao se id for maior que 0.
+            this._id = id
+        }
+    }
 }
 
 const c1 = new Curso()
+c1.id = -123
 c1.nome = 'Iniciando com TypeScript'
+console.log(c1.id)
 console.log(c1.nome)
-
-
-const c2 = new Curso()
-console.log(c2.nome)
