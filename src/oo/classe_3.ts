99521459688cb0d4224b620276efb8942401f535
PedroLMaia <pedro-luiz-maia@hotmail.com>
2023-09-03 19:01:12 -0300

    usando metodo static

--- src/oo/classe_3.ts
+++ src/oo/classe_3.ts
@@ -1,15 +1,16 @@
+import Validador from "./Validador"
+
 class Produto {
     constructor(
-        readonly id: number, 
-        readonly nome: string, 
-        readonly preco: number, 
+        readonly id: number,
+        readonly nome: string,
+        readonly preco: number,
         readonly desc: number = 0
     ) {
-        if(id <= 0) throw new Error('Id inválido')
-        if(nome.length < 2) throw new Error('Nome pequeno')
-		if(nome.length > 250) throw new Error('Nome grande')
-		if(preco <= 0) throw new Error('Preço inválido')
-		if(desc < 0 || desc > 0.8) throw new Error('Desconto inválido')
+        Validador.maiorQueZero(id, 'Id inválido')
+        Validador.tamanhoEntre(nome, 3, 250, 'Nome inválido')
+        Validador.maiorQueZero(preco, 'Preço inválido')
+        Validador.entre(desc, 0, 0.8, 'Desconto inválido')
     }
 
     get precoFinal() {
@@ -18,8 +19,11 @@ class Produto {
 }
 
 const p1 = new Produto(1, 'Caneta', 12.5, 0.1)
-const p2 = new Produto(2, 'Notebook', 7860.78)
-console.log(p1.nome ,p1.precoFinal)
-console.log(p2.nome ,p2.precoFinal)
+console.log(p1.nome, p1.precoFinal)
 
-export {}
+const p2 = new Produto(2, 'Notebook', 7860.78)
+console.log(p2.nome, p2.preco)
+
+console.log(Validador.ERRO_DESCONHECIDO)
+
+export { }
