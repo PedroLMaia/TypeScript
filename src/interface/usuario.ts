9ec4052d7aa8927abaf187b04b5c27f5e80c1009
PedroLMaia <pedro-luiz-maia@hotmail.com>
2023-09-04 16:10:59 -0300

    interface com atributo opcional

--- src/interface/usuario.ts
+++ src/interface/usuario.ts
@@ -2,16 +2,19 @@ interface Usuario {
     id: number
     nome: string
     email: string
+    senha?: string
 }
 
 const usuarioLogado: Usuario = {
     id: 123,
     nome: 'João',
-    email: 'joao@zmail.com'
+    email: 'joao@zmail.com',
+    senha: '123456'
 }
 
 console.log(usuarioLogado.id)
 console.log(usuarioLogado.nome)
 console.log(usuarioLogado.email)
+console.log(usuarioLogado.senha)
 
 export {}
