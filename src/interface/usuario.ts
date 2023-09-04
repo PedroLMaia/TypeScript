d4cb5b730b9855c40a2ebf2419ad4f1a06ebc197
PedroLMaia <pedro-luiz-maia@hotmail.com>
2023-09-04 16:37:12 -0300

    usando os tipos de utilitários do typescript

--- src/interface/usuario.ts
+++ src/interface/usuario.ts
@@ -1,5 +1,5 @@
 interface Usuario {
-    id: number
+    id?: number
     nome: string
     email: string
     senha?: string
@@ -15,6 +15,28 @@ const usuarioLogado: Usuario = {
 console.log(usuarioLogado.id)
 console.log(usuarioLogado.nome)
 console.log(usuarioLogado.email)
-console.log(usuarioLogado.senha)
+
+const usuarioOpcional: Partial<Usuario> = {
+    id: 123,
+    nome: 'Jaime',
+    senha: '654321'
+}
+
+console.log(usuarioOpcional.id)
+console.log(usuarioOpcional.nome)
+console.log(usuarioOpcional.email)
+console.log(usuarioOpcional.senha)
+
+const usuarioCompleto: Required<Usuario> = {
+    id: 123,
+    nome: 'Ana',
+    email: 'ana@ana.land',
+    senha: '123456'
+}
+
+console.log(usuarioCompleto.id)
+console.log(usuarioCompleto.nome)
+console.log(usuarioCompleto.email)
+console.log(usuarioCompleto.senha)
 
 export {}
